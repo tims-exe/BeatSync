b0fcdfcc282f5a12fdb464542b350199e5004e02
tims-exe <timonkjohn@gmail.com>
2024-06-28 23:54:48 +0530

    frontend react

--- beatsync/urls.py
+++ beatsync/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))   # asks the main urls file to include urls from "api" at that endpoint
+    path('api/', include('api.urls')),   # asks the main urls file to include urls from "api" at that endpoint
+    path('', include('frontend.urls')),  
 ]
