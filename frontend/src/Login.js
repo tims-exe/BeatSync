1784eb51d45916d43e1d312e7fb82d45e8c75f80
tims-exe <timonkjohn@gmail.com>
2024-07-02 21:53:13 +0530

    dependencies and structure

---
+++ frontend/src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=a8d740f0b9d44275948b29635c04f387&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+const Login = () => {
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Login
