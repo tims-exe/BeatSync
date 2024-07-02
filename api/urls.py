e909ba95ad05851f12118346cac5654046262018
tims-exe <timonkjohn@gmail.com>
2024-07-02 21:36:01 +0530

    updating beatsync

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import RoomView
-
-urlpatterns = [
-    path('room', RoomView.as_view())     # calls the main function inside views of "api" at the given endpoint 
-]
